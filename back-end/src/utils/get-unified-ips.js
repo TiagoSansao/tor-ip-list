9e810d9f0fa4c65cc0be4fbc0c1f180e98ffd693
Tiago Sansão <tiagossansao@gmail.com>
2021-09-06 16:20:47 -0300

    Finished get-unified-ips module and added a way to avoid an error if the API doesn't return a corrent response

--- back-end/src/utils/get-unified-ips.js
+++ back-end/src/utils/get-unified-ips.js
@@ -1,21 +1,33 @@
 import axios from 'axios';
+import fs from 'fs/promises';
+
+
 
 async function loadIps() {
   
-  const urls = ["https://onionoo.torproject.org/summary?limit=5000", "https://www.dan.me.uk/tornodes"];
-  const ips = [];
+  const urls = ["https://onionoo.torproject.org/summary?limit=5000", "https://www.dan.me.uk/torlist"];
+  let ips = [];
 
   // Store IPS from the first address
 
   const response_01 = await axios.get(urls[0]);
+  // const response_02 = await axios.get(urls[1]);
+
   response_01.data.relays.forEach((relay) => {
     ips.push(relay.a[0]);
   });
 
-  console.log(ips);
-
   // Store IPS from the second address
 
+  //  DUE TO THE DAN API LIMITATIONS, IT IS ONLY POSSIBLE TO REQUEST DATA ONCE IN THIRTY MINUTES
+  //  SO, I DOWNLOADED A SAMPLE RESPONSE TO BUILD THE APPLICATION
+
+  const response_02_data = await fs.readFile("./src/utils/www.dan.me.uk.txt", "utf-8");
+  ips = ips.concat(response_02_data.split("\n"));
+
+  // ips = ips.concat(response_02.data.split("\n"));
+
+  return ips;
 };
 
 export default loadIps;
