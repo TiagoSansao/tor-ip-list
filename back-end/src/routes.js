b141785088016da8f9476363a86502d38fb0fa82
Tiago Sansão <tiagossansao@gmail.com>
2021-09-06 20:54:11 -0300

    Improved api responses

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,42 @@
 import Router from 'express';
+import Blacklist from './database/models/blacklist.js';
+import getUnifiedIps from './utils/get-unified-ips.js';
 
+const unifiedIps = await getUnifiedIps();
 const routes = Router();
 
-routes.get("/ips");
-routes.get("/filtered-ips");
-routes.post("/filter-ip");
+routes.get("/ips", (req, res) => {
+  res.status(200).json(unifiedIps);
+});
+
+routes.get("/filtered-ips", async (req, res) => {
+  const blacklistedIps = [];
+  const savedIps = [];
+  const unifiedIpsWithoutTheSavedIps = [...unifiedIps];
+  const savedIpsObj = await Blacklist
+  .find()
+  .select({_id: 0, __v: 0})
+  .exec();
+  for (let key in savedIpsObj) {
+    savedIps.push(savedIpsObj[key].ip);
+  };
+  savedIps.forEach((savedIp) => {
+    const index = unifiedIpsWithoutTheSavedIps.indexOf(savedIp);
+    if (index !== -1) {
+      blacklistedIps.push(unifiedIpsWithoutTheSavedIps[index]);
+      unifiedIpsWithoutTheSavedIps.splice(index, 1);
+    };
+  });
+  res.status(200).json({ips: unifiedIpsWithoutTheSavedIps, blacklistedIps});
+});
+
+routes.post("/filter-ip", (req, res) => {
+  const ip = req.body.ip;
+  const newBlacklistedIp = new Blacklist({ ip: ip });
+  newBlacklistedIp.save((err, result) => {
+    if (err) res.status(400).json({status: "Bad request!"});
+    else res.status(200).json({status: `Successfully added IP: ${ip} to the blacklist!`});
+  });
+});
 
 export default routes;
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,19 +1,13 @@
-// TODO ->
-// Send the routes from this file to the routes.js 
-
-
 // IMPORTS AND CONFIGS
 
 import express from 'express';
 import cors from 'cors';
-import getUnifiedIps from './utils/get-unified-ips.js';
-import Blacklist from './database/models/blacklist.js';
 import connect from "./database/connect.js";
-// import routes from './routes';
+import routes from './routes.js';
 import {} from 'dotenv/config';
 
 const app = express();
-const unifiedIps = await getUnifiedIps();
+
 const port = process.env.PORT || 5555;
 
 // CONNECT TO DATABASE
@@ -23,48 +17,11 @@ connect();
 // MIDDLEWARES
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-// app.use(routes); 
+app.use(routes); 
 
-
-//
-
-app.get("/ips", (req, res) => {
-  res.status(200).json(unifiedIps);
-});
-
-app.get("/filtered-ips", async (req, res) => {
-  const savedIps = [];
-  const savedIpsObj = await Blacklist
-  .find()
-  .select({_id: 0, __v: 0})
-  .exec();
-  for (let key in savedIpsObj) {
-    savedIps.push(savedIpsObj[key].ip);
-  }
-  console.log(savedIps);
-  savedIps.forEach((savedIp) => {
-    const index = unifiedIps.indexOf(savedIp);
-    if (index !== -1) {
-      unifiedIps.splice(index, 1);
-      console.log(index);
-    };
-  });
-  res.status(200).json(unifiedIps).send();
-});
-
-app.post("/filter-ip", (req, res) => {
-  const ip = req.body.ip;
-  const newBlacklistedIp = new Blacklist({ ip: ip });
-  newBlacklistedIp.save((err, result) => {
-    if (err) res.status(400).json({status: "Bad request!"});
-    else res.status(200).json({status: "IP added successfully"});
-  })
-});
-
-
-//
+// TURNING ON 
 
 app.listen(port, () => {
   console.log(`TOR IP LIST's back-end running on port: ${port}!`);
