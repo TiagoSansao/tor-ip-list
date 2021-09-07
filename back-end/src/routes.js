6d9d6bcd14ffbcedf614184a09103f3d8fb6f70f
Tiago Sansão <tiagossansao@gmail.com>
2021-09-07 14:41:53 -0300

    Created new endpoint: unblacklist-ip

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -30,13 +30,27 @@ routes.get("/filtered-ips", async (req, res) => {
   res.status(200).json({ips: unifiedIpsWithoutTheSavedIps, blacklistedIps});
 });
 
-routes.post("/filter-ip", (req, res) => {
+routes.post("/blacklist-ip", (req, res) => {
   const ip = req.body.ip;
   if (!ip) return res.status(400).send();
   const newBlacklistedIp = new Blacklist({ ip: ip });
   newBlacklistedIp.save((err, result) => {
-    if (err) res.status(400).json({status: "Bad request!"});
-    else res.status(200).json({status: `Successfully added IP: ${ip} to the blacklist!`});
+    if (err) res.status(400).json({status: "Bad request"});
+    else res.status(200).json({status: `Successfully added IP: ${ip} to the blacklist`});
+  });
+});
+
+routes.delete("/unblacklist-ip", (req, res) => {
+  const ip = req.body.ip;
+  if (!ip) return res.status(400).send();
+  Blacklist.find({ip: ip}).deleteMany((err, result) => {
+    if (err) console.log(err);
+    if (result.deleteCount === 0) res.status(204).json({status: `Error: the IP: ${ip} wasn't blacklisted.`}) 
+    else  res.status(200).json({status: `Successfully removed IP: ${ip} from the backlist.`}) 
+
+    
+    
+    console.log(result);
   });
 });
 
