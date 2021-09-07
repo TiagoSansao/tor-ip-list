2d3c49dbb0634102fd4318323d9aea9728cb348e
Tiago Sansão <tiagossansao@gmail.com>
2021-09-07 18:39:47 -0300

    Front-end feedbacks now are colored. Green whenever ther esult is positive and red whenever the result is negative

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import api from "./services/api";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -6,6 +6,8 @@ import { AxiosResponse } from "axios";
 import "./styles/css/app.css";
 import "./styles/css/global.css";
 
+// TODO
+// Focus at element after submitting
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
   const [removeIp, setRemoveIp] = useState<string>("");
   const [addIpFeedback, setAddIpFeedback] = useState<string>("")
   const [removeIpFeedback, setRemoveIpFeedback] = useState<string>("")
+  const [getIpsFeedback, setGetIpsFeedback] = useState<string>("")
+  const [getFilteredIpsFeedback, setGetFilteredIpsFeedback] = useState<string>("")
   const [responseIps, setResponseIps] = useState({ ips: [] })
 
   async function blacklistIpHandler(event: React.FormEvent) {
@@ -45,15 +49,22 @@ function App() {
   async function getAllIpsHandler(event: React.FormEvent) {
     event.preventDefault();
     const response = await api.get("ips");
-    console.log(response.data);
     setResponseIps(response.data);
+    setGetIpsFeedback("Successfully retrieved list of all IPS")
+    setResponseIps(response.data);
+    setTimeout(() => {
+      setGetIpsFeedback("");
+    }, 3000)
   };
 
   async function getFilteredIpsHandler(event: React.FormEvent) {
     event.preventDefault();
     const response = await api.get("filtered-ips");
-    console.log(response.data);
+    setGetFilteredIpsFeedback("Successfully retrieved filtered list of IPS")
     setResponseIps(response.data);
+    setTimeout(() => {
+      setGetFilteredIpsFeedback("");
+    }, 3000)
 
   };
 
@@ -68,24 +79,26 @@ function App() {
               <p>IPS submitted in this form won't show up in the filtered-ips endpoint</p>
               <input type="text" value={ip} placeholder={"x.xxx.xx.xx"} required onChange={(e) => setIp(e.target.value)} />
               <input type="submit" /> <br />
-              <span id="blacklist-ip-feedback">{addIpFeedback}&nbsp;</span>
+              <span style={{ color: /Error:/.test(addIpFeedback) ? "red" : "green" }} id="blacklist-ip-feedback">{addIpFeedback}&nbsp;</span>
             </form>
             <form id="unblacklist-ip" onSubmit={unblacklistIpHandler}>
-              <h3>Remove an IP from the backlist</h3>
-              <p>IPS submitted in this form will be removed from the backlist</p>
+              <h3>Remove an IP from the blacklist</h3>
+              <p>IPS submitted in this form will be removed from the blacklist</p>
               <input type="text" value={removeIp} placeholder={"x.xxx.xx.xx"} required onChange={(e) => setRemoveIp(e.target.value)} />
               <input type="submit" /> <br />
-              <span id="blacklist-ip-feedback">{removeIpFeedback}&nbsp;</span>
+              <span style={{ color: /Error:/.test(removeIpFeedback) ? "red" : "green" }} id="blacklist-ip-feedback">{removeIpFeedback}&nbsp;</span>
             </form>
             <form id="get-ips" onSubmit={getAllIpsHandler}>
               <h3>Get all IPS</h3>
               <p>Retrieve a list of all TOR IPS</p>
-              <input type="submit" value="Get IPS" />
+              <input type="submit" value="Get IPS" /> <br />
+              <span style={{ color: "green" }} id="blacklist-ip-feedback">{getIpsFeedback}&nbsp;</span>
             </form>
             <form id="get-filtered-ips" onSubmit={getFilteredIpsHandler}>
               <h3>Get only IPS which aren't blacklisted</h3>
               <p>Retrieve a list of TOR IPS without the ones which are blacklisted</p>
-              <input type="submit" value="Get filtered IPS" />
+              <input type="submit" value="Get filtered IPS" /> <br />
+              <span style={{ color: "green" }} id="blacklist-ip-feedback">{getFilteredIpsFeedback}&nbsp;</span>
             </form>
           </main>
           <aside id="panel">
