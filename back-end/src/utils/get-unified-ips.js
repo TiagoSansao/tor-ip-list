1580d78fb8f8cadb00de54038896bcf1752a7262
Tiago Sansão <tiagossansao@gmail.com>
2021-09-12 10:34:43 -0300

    Last minor changes, everything is working successfully

--- back-end/src/utils/get-unified-ips.js
+++ back-end/src/utils/get-unified-ips.js
@@ -11,7 +11,7 @@ async function loadIps() {
   // Store IPS from the first address
 
   const response_01 = await axios.get(urls[0]);
-  // const response_02 = await axios.get(urls[1]);
+  const response_02 = await axios.get(urls[1]);
 
   response_01.data.relays.forEach((relay) => {
     ips.push(relay.a[0]);
@@ -22,10 +22,10 @@ async function loadIps() {
   //  DUE TO THE DAN API LIMITATIONS, IT IS ONLY POSSIBLE TO REQUEST DATA ONCE IN THIRTY MINUTES
   //  SO, I DOWNLOADED A SAMPLE RESPONSE TO BUILD THE APPLICATION
 
-  const response_02_data = await fs.readFile("./src/utils/www.dan.me.uk.txt", "utf-8");
-  ips = ips.concat(response_02_data.split("\n"));
+  // const response_02_data = await fs.readFile("./src/utils/www.dan.me.uk.txt", "utf-8");
+  // ips = ips.concat(response_02_data.split("\n"));
 
-  // ips = ips.concat(response_02.data.split("\n"));
+  ips = ips.concat(response_02.data.split("\n"));
 
   return ips;
 };
